467c066191634bdbd3167770214bf8052460064d
Arvind Jayaprakash <work@anomalizer.net>
2012-07-10 16:35:16 +0530

    crude impl of invocation tracking

--- django_views_stats/Aggregator.py
+++ django_views_stats/Aggregator.py
@@ -6,15 +6,50 @@ class ViewsStatsMiddleware(object):
         print 'processing request'
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        view_name = '%s.%s' % (view_func.__module__, view_func.__name__) 
-        request.__view_name = view_name
-        print 'processing view %s' % view_name
+        request.__view = view_func
 
     def process_response(self, request, response):
+        view_func = None
         try:
-            view_name = request.__view_name
-            print 'processing response %s' % view_name
+            view_func = request.__view
+            print 'processing response %s' % view_func
         except AttributeError:
+            view_func = unknown
             print 'aieee, no view for you'
-        finally:
-            return response
+
+        if(view_func in self.view_map):
+            counter = self.view_map[view_func]
+        else:
+            counter = HttpStatusCounter()
+            self.view_map[view_func] = counter
+        
+        counter.record_request(response.status_code, 1) #TODO: real time taken
+        for k, v in self.view_map.iteritems():
+            print "%s => %s" % (k, v)
+        return response
+
+
+def unknown():
+    pass
+
+class HttpStatusCounter(object):
+    def __init__(self):
+        self.code_count = {}
+        self.code_time = {}
+
+    def record_request(self, status_code, millis):
+        if(status_code not in self.code_count):
+            self.code_count[status_code] = 1
+            self.code_time[status_code] = millis
+        else:
+            self.code_count[status_code] = self.code_count[status_code] + 1
+            self.code_time[status_code] = self.code_time[status_code] + millis
+
+    def get_counts(self):
+        return self.code_count
+
+    def get_cumulative_duration(self):
+        return self.code_time
+
+    def __str__(self):
+        return 'invocations: %s, cumulative millis %s' % (self.code_count, self.code_time)
